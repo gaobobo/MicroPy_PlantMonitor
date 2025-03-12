f121bafb7050fd1e82b48ca18269a693abe4a781
Bobby Gao <32958058+gaobobo@users.noreply.github.com>
2025-03-12 22:41:15 +0800

    wip(lcd): add struct of 8pins HAL
    TODO: complete 8pins HAL. Ref 4pins.

---
+++ library/HD44780_LCD_Driver/HAL/ABC_GPIO8_HAL.py
@@ -0,0 +1,18 @@
+from abc import abstractmethod, ABC
+
+
+class GPIO4_HAL(ABC):
+    #TODO: complete 8pins HAL. Ref 4pins.
+    # must at least below func.
+
+    def __init__(self):
+        pass
+
+    def write(self):
+        pass
+
+    def write_int(self):
+        pass
+
+    def read(self):
+        pass
