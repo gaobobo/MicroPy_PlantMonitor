ddc04fb2fe4e9277d535c99d1b423e5806f856e3
Bobby Gao <32958058+gaobobo@users.noreply.github.com>
2025-03-12 22:21:39 +0800

    feat(lcd): achieve pyboard HAL of 4pins

---
+++ library/HD44780_LCD_Driver/HAL/pyboard/pyb_GPIO4_HAL.py
@@ -0,0 +1,23 @@
+from library.HD44780_LCD_Driver.HAL.ABC_GPIO4_HAL import *
+from pyb import Pin
+
+class pyb_GPIO4_HAL(GPIO4_HAL):
+
+    def _init_pin_in(self, pin: Pin):
+        pin.init(mode=Pin.IN)
+
+    def _init_pin_out(self, pin: Pin):
+        pin.init(mode=Pin.OUT_PP)
+
+    def _write_to_pin(self, pin: Pin, is_high: bool):
+        pin.high() if is_high else pin.low()
+
+    def _read_from_pin(self, pin: Pin) -> int:
+        return pin.value()
+
+    def _delay(self, cycle: int):
+        super()._delay(cycle)
+
+    def __init__(self, RS:Pin, RW:Pin,
+                 E:Pin, DB4:Pin, DB5:Pin, DB6:Pin, DB7:Pin):
+        super().__init__(RS, RW, E, DB4, DB5, DB6, DB7)
