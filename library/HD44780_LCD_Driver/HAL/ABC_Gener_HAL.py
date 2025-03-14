c3fcd12c714e2d51ff8385d0904798eb45a7b9d3
Bobby Gao <32958058+gaobobo@users.noreply.github.com>
2025-03-14 09:56:24 +0800

    feat(lcd): add abstract HAL of general

---
+++ library/HD44780_LCD_Driver/HAL/ABC_Gener_HAL.py
@@ -0,0 +1,14 @@
+# Copyright (c) Gao Shibo. All rights reserved.
+# Licensed under The MIT License, see LICENSE in repo's root
+
+from abc import ABC, abstractmethod
+
+class General_HAL (ABC):
+
+    @abstractmethod
+    def write(self, RS_level: int, RW_level: int, DBs_level: int, delay_cycles:int = 1):
+        pass
+
+    @abstractmethod
+    def read(self, RS_level:int, RW_level:int) -> int:
+        pass
