9acbb1d2efcffbce80df6a6617c19ca1099382ce
Bobby Gao <32958058+gaobobo@users.noreply.github.com>
2025-03-21 17:24:50 +0800

    chore(lcd): add file description

--- library/HD44780_Driver/HAL/ABC_GPIO4_HAL.py
+++ library/HD44780_Driver/HAL/ABC_GPIO4_HAL.py
@@ -1,6 +1,12 @@
 # Copyright (c) Gao Shibo. All rights reserved.
 # Licensed under the MIT License, see LICENSE in repo's root
 
+"""
+**Hardware of GPIO4**
+
+Extend this class to use RS, RW, E, DB4, DB5, DB6, DB7 to communicate with hardware.
+"""
+
 from machine import Pin
 from time import sleep_us, sleep_ms
 from .ABC_Gener_HAL import General_HAL
