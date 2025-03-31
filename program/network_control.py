a6c50a4d404a827557f0913090a505919ee3d4a7
Bobby Gao <32958058+gaobobo@users.noreply.github.com>
2025-03-31 18:02:36 +0800

    fix(program): args self not found

--- program/network_control.py
+++ program/network_control.py
@@ -11,57 +11,60 @@ class NetworkWrongPasswordError(NetworkError): pass
 class NetworkNoAPFoundError(NetworkError): pass
 
 
-def disconnect(self):
-    self.wlan.disconnect()
+def disconnect(wlan:WLAN) -> None:
+    wlan.disconnect()
 
 
-def connect(self, ssid:str, password:str) -> None:
+def connect(wlan:WLAN, ssid:str, password:str) -> None:
     """Must disconnect first when connect others network.
     This func is not block, so you can check the connection status
-    use self.wlan.status() ."""
-    self.wlan.active(True)
+    use wlan.status() ."""
+    wlan.active(True)
 
-    if self.wlan.isconnected(): return
+    if wlan.isconnected(): return
 
-    self.wlan.connect(ssid, password)
+    wlan.connect(ssid, password)
 
 
-async def async_connect(self, refresh_cycle_sec:float) -> None:
+async def async_connect(wlan:WLAN, ssid:str, password:str, refresh_cycle_sec:float) -> None:
     """
     Must disconnect first when connect others network.
 
     This func is async and will throw exception when wlan is not connected and
     connect successfully return None.
+    :param wlan: WLAN Object
+    :param ssid: connect ssid
+    :param password: connect password
     :param refresh_cycle_sec: seconds to check connect status per cycle. Support float.
     """
-    self.wlan.active(True)
+    wlan.active(True)
 
-    if self.wlan.isconnected(): return
+    if wlan.isconnected(): return
 
-    self.wlan.connect(self.ssid, self.password)
+    wlan.connect(ssid, password)
 
-    while self.wlan.status() == STAT_CONNECTING:
+    while wlan.status() == STAT_CONNECTING:
         await sleep(refresh_cycle_sec)
 
-    if self.wlan.status() == STAT_GOT_IP:
+    if wlan.status() == STAT_GOT_IP:
         return
-    elif self.wlan.status() == STAT_WRONG_PASSWORD:
+    elif wlan.status() == STAT_WRONG_PASSWORD:
         raise NetworkWrongPasswordError()
-    elif self.wlan.status() == STAT_NO_AP_FOUND:
+    elif wlan.status() == STAT_NO_AP_FOUND:
         raise NetworkNoAPFoundError()
     else:
         raise NetworkError()
 
 
-def get_level(self) -> int|None :
+def get_level(wlan:WLAN) -> int|None :
     """
     return rssi of WI-FI.
     :return: an int value of rssi. > -50 is 2, > -70 is 1, else is 0, disconnect is None.
     """
 
-    if not self.wlan.isconnected(): return None
+    if not wlan.isconnected(): return None
 
-    rssi = self.wlan.status('rssi')
+    rssi = wlan.status('rssi')
 
     if rssi > -50: return 2
     elif rssi > -70: return 1
